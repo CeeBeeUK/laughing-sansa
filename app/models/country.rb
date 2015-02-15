d2c399fe6dd72b25e9091f5a8d2827436096084b
Colin Bruce <colinbruce@gmail.com>
2015-02-15 22:37:39 +0000

    Updated image path handling

--- app/models/country.rb
+++ app/models/country.rb
@@ -5,19 +5,19 @@ class Country < ActiveRecord::Base
   validates :name, presence: true
 
   def small_image
-    "assets/16/#{get_image_path}.png"
+    "/assets/16/#{get_image_path}.png"
   end
 
   def large_image
-    "assets/32/#{get_image_path}.png"
+    "/assets/32/#{get_image_path}.png"
   end
 
   def xl_image
-    "assets/64/#{get_image_path}.png"
+    "/assets/64/#{get_image_path}.png"
   end
 
   def disabled_image
-    "assets/64/#{get_image_path}_bw.png"
+    "/assets/64/#{get_image_path}_bw.png"
   end
 
   private
--- spec/models/country_spec.rb
+++ spec/models/country_spec.rb
@@ -11,20 +11,20 @@ RSpec.describe Country, :type => :model do
 
   context 'with no image set' do
     it 'should return a small image path from the country name' do
-      expect(country.small_image).to eql('assets/16/united_kingdom.png')
+      expect(country.small_image).to eql('/assets/16/united_kingdom.png')
     end
 
     it 'should return a large image path from the country name' do
-      expect(country.large_image).to eql('assets/32/united_kingdom.png')
+      expect(country.large_image).to eql('/assets/32/united_kingdom.png')
     end
 
     it 'should return a XL image path from the country name' do
-      expect(country.xl_image).to eql('assets/64/united_kingdom.png')
+      expect(country.xl_image).to eql('/assets/64/united_kingdom.png')
     end
 
     context 'requesting disabled badge' do
       it 'should return a b&w large image from the country name' do
-        expect(country.disabled_image).to eql('assets/64/united_kingdom_bw.png')
+        expect(country.disabled_image).to eql('/assets/64/united_kingdom_bw.png')
       end
     end
   end
