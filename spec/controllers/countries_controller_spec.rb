c655542affbf1cb54283a5e30764fd4685f819cc
Colin Bruce <colinbruce@gmail.com>
2015-02-17 19:16:32 +0000

    updated specs for countries

--- spec/controllers/countries_controller_spec.rb
+++ spec/controllers/countries_controller_spec.rb
@@ -18,124 +18,128 @@ RSpec.describe CountriesController, :type => :controller do
   let(:user) { FactoryGirl.create :user }
   let(:admin_user) { FactoryGirl.create :admin_user }
   
-  describe 'GET index' do
-    it 'assigns all countries as @countries' do
-      country = Country.create! valid_attributes
-      get :index, {}, valid_session
-      expect(assigns(:countries)).to include(country)
-    end
-  end
+  context 'as a logged in admin' do
 
-  describe 'GET show' do
-    it 'assigns the requested country as @country' do
-      country = Country.create! valid_attributes
-      get :show, {:id => country.to_param}, valid_session
-      expect(assigns(:country)).to eq(country)
-    end
-  end
+    before(:each) { sign_in admin_user }
 
-  describe 'GET new' do
-    it 'assigns a new country as @country' do
-      get :new, {}, valid_session
-      expect(assigns(:country)).to be_a_new(Country)
-    end
-  end
-
-  describe 'GET edit' do
-    it 'assigns the requested country as @country' do
-      country = Country.create! valid_attributes
-      get :edit, {:id => country.to_param}, valid_session
-      expect(assigns(:country)).to eq(country)
-    end
-  end
-
-  describe 'POST create' do
-    describe 'with valid params' do
-      it 'creates a new Country' do
-        expect {
-          post :create, {:country => valid_attributes}, valid_session
-        }.to change(Country, :count).by(1)
-      end
-
-      it 'assigns a newly created country as @country' do
-        post :create, {:country => valid_attributes}, valid_session
-        expect(assigns(:country)).to be_a(Country)
-        expect(assigns(:country)).to be_persisted
-      end
-
-      it 'redirects to the created country' do
-        post :create, {:country => valid_attributes}, valid_session
-        expect(response).to redirect_to(Country.last)
-      end
-    end
-
-    describe 'with invalid params' do
-      it 'assigns a newly created but unsaved country as @country' do
-        post :create, {:country => invalid_attributes}, valid_session
-        expect(assigns(:country)).to be_a_new(Country)
-      end
-
-      it 're-renders the "new" template' do
-        post :create, {:country => invalid_attributes}, valid_session
-        expect(response).to render_template('new')
-      end
-    end
-  end
-
-  describe 'PUT update' do
-    describe 'with valid params' do
-      let(:new_attributes) {
-        { name: 'uk' }
-      }
-
-      it 'updates the requested country' do
+    describe 'GET index' do
+      it 'assigns all countries as @countries' do
         country = Country.create! valid_attributes
-        put :update, {:id => country.to_param, :country => new_attributes}, valid_session
-        country.reload
-        expect(country.name).to eql('uk')
+        get :index, {}, valid_session
+        expect(assigns(:countries)).to include(country)
       end
+    end
 
+    describe 'GET show' do
       it 'assigns the requested country as @country' do
         country = Country.create! valid_attributes
-        put :update, {:id => country.to_param, :country => valid_attributes}, valid_session
+        get :show, {:id => country.to_param}, valid_session
         expect(assigns(:country)).to eq(country)
-      end
-
-      it 'redirects to the country' do
-        country = Country.create! valid_attributes
-        put :update, {:id => country.to_param, :country => valid_attributes}, valid_session
-        expect(response).to redirect_to(country)
       end
     end
 
-    describe 'with invalid params' do
-      it 'assigns the country as @country' do
-        country = Country.create! valid_attributes
-        put :update, {:id => country.to_param, :country => invalid_attributes}, valid_session
-        expect(assigns(:country)).to eq(country)
-      end
-
-      it 're-renders the "edit" template' do
-        country = Country.create! valid_attributes
-        put :update, {:id => country.to_param, :country => invalid_attributes}, valid_session
-        expect(response).to render_template('edit')
+    describe 'GET new' do
+      it 'assigns a new country as @country' do
+        get :new, {}, valid_session
+        expect(assigns(:country)).to be_a_new(Country)
       end
     end
-  end
 
-  describe 'DELETE destroy' do
-    it 'destroys the requested country' do
-      country = Country.create! valid_attributes
-      expect {
+    describe 'GET edit' do
+      it 'assigns the requested country as @country' do
+        country = Country.create! valid_attributes
+        get :edit, {:id => country.to_param}, valid_session
+        expect(assigns(:country)).to eq(country)
+      end
+    end
+
+    describe 'POST create' do
+      describe 'with valid params' do
+        it 'creates a new Country' do
+          expect {
+            post :create, {:country => valid_attributes}, valid_session
+          }.to change(Country, :count).by(1)
+        end
+
+        it 'assigns a newly created country as @country' do
+          post :create, {:country => valid_attributes}, valid_session
+          expect(assigns(:country)).to be_a(Country)
+          expect(assigns(:country)).to be_persisted
+        end
+
+        it 'redirects to the created country' do
+          post :create, {:country => valid_attributes}, valid_session
+          expect(response).to redirect_to(Country.last)
+        end
+      end
+
+      describe 'with invalid params' do
+        it 'assigns a newly created but unsaved country as @country' do
+          post :create, {:country => invalid_attributes}, valid_session
+          expect(assigns(:country)).to be_a_new(Country)
+        end
+
+        it 're-renders the "new" template' do
+          post :create, {:country => invalid_attributes}, valid_session
+          expect(response).to render_template('new')
+        end
+      end
+    end
+
+    describe 'PUT update' do
+      describe 'with valid params' do
+        let(:new_attributes) {
+          { name: 'uk' }
+        }
+
+        it 'updates the requested country' do
+          country = Country.create! valid_attributes
+          put :update, {:id => country.to_param, :country => new_attributes}, valid_session
+          country.reload
+          expect(country.name).to eql('uk')
+        end
+
+        it 'assigns the requested country as @country' do
+          country = Country.create! valid_attributes
+          put :update, {:id => country.to_param, :country => valid_attributes}, valid_session
+          expect(assigns(:country)).to eq(country)
+        end
+
+        it 'redirects to the country' do
+          country = Country.create! valid_attributes
+          put :update, {:id => country.to_param, :country => valid_attributes}, valid_session
+          expect(response).to redirect_to(country)
+        end
+      end
+
+      describe 'with invalid params' do
+        it 'assigns the country as @country' do
+          country = Country.create! valid_attributes
+          put :update, {:id => country.to_param, :country => invalid_attributes}, valid_session
+          expect(assigns(:country)).to eq(country)
+        end
+
+        it 're-renders the "edit" template' do
+          country = Country.create! valid_attributes
+          put :update, {:id => country.to_param, :country => invalid_attributes}, valid_session
+          expect(response).to render_template('edit')
+        end
+      end
+    end
+
+    describe 'DELETE destroy' do
+      it 'destroys the requested country' do
+        country = Country.create! valid_attributes
+        expect {
+          delete :destroy, {:id => country.to_param}, valid_session
+        }.to change(Country, :count).by(-1)
+      end
+
+      it 'redirects to the countries list' do
+        country = Country.create! valid_attributes
         delete :destroy, {:id => country.to_param}, valid_session
-      }.to change(Country, :count).by(-1)
-    end
-
-    it 'redirects to the countries list' do
-      country = Country.create! valid_attributes
-      delete :destroy, {:id => country.to_param}, valid_session
-      expect(response).to redirect_to(countries_url)
+        expect(response).to redirect_to(countries_url)
+      end
     end
   end
-
 end
--- spec/requests/countries_spec.rb
+++ spec/requests/countries_spec.rb
@@ -1,10 +1,30 @@
 require 'rails_helper'
 
 RSpec.describe "Countries", :type => :request do
+
+  include Warden::Test::Helpers
+  Warden.test_mode!
+
+  let(:user) { FactoryGirl.create :user }
+  let(:admin_user) { FactoryGirl.create :admin_user }
+
   describe "GET /countries" do
-    it "works! (now write some real specs)" do
+    it "Redirects us when not authenticated." do
       get countries_path
-      expect(response).to have_http_status(200)
+      expect(response.status).to eql(302)
     end
+
+    it "Redirects us when not authorised." do
+      login_as(user, :scope => :user)
+      get countries_path
+      expect(response.status).to eql(302)
+    end
+
+    it "Gives is the expected status code when authenticated." do
+      login_as(admin_user, :scope => :user)
+      get countries_path
+      expect(response.status).to eql(200)
+    end
+
   end
 end
