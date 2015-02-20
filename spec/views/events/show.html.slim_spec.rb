ce00cc1bc127fd715e0736f8b127e521e317aa87
Colin Bruce <colinbruce@gmail.com>
2015-02-20 21:24:50 +0000

    Updated display and specs

--- spec/views/events/show.html.slim_spec.rb
+++ spec/views/events/show.html.slim_spec.rb
@@ -1,15 +1,21 @@
 require 'rails_helper'
 
-RSpec.describe "events/show", type: :view do
+RSpec.describe 'events/show', type: :view do
+
+  include Devise::TestHelpers
+
+  let(:user) { FactoryGirl.create :user }
+
   before(:each) do
     @event = FactoryGirl.create :event
+    @event.country = FactoryGirl.create :country
   end
 
-  it "renders attributes in <p>" do
+  it 'renders attributes' do
+    sign_in user
     render
     expect(rendered).to match(/#{@event.year}/)
-    expect(rendered).to match(/Host city/)
-    expect(rendered).to match(/false/)
-    expect(rendered).to match(//)
+    expect(rendered).to match(/london/)
+
   end
 end
