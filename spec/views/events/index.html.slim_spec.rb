075e151768ec089252223de6b9174b243146d9a7
Colin Bruce <colinbruce@gmail.com>
2015-02-20 10:39:04 +0000

    updated specs

--- spec/views/events/index.html.slim_spec.rb
+++ spec/views/events/index.html.slim_spec.rb
@@ -1,28 +1,28 @@
 require 'rails_helper'
 
-RSpec.describe "events/index", type: :view do
+RSpec.describe 'events/index', type: :view do
   before(:each) do
     assign(:events, [
       Event.create!(
         :year => 2010,
-        :host_city => "Host City",
+        :host_city => 'Host City',
         :active => false,
         :country_id => 1
       ),
       Event.create!(
         :year => 2011,
-        :host_city => "Host City",
+        :host_city => 'Host City',
         :active => false,
         :country_id => 2
       )
     ])
   end
 
-  it "renders a list of events" do
+  it 'renders a list of events' do
     render
-    assert_select "li>span.year", :text => 2010.to_s, :count => 1
-    assert_select "li>span.year", :text => 2011.to_s, :count => 1
-    assert_select "li>span.host", :text => "Host City".to_s, :count => 2
-    assert_select "tr>td", :text => false.to_s, :count => 2
+    assert_select 'li>span.year', :text => 2010.to_s, :count => 1
+    assert_select 'li>span.year', :text => 2011.to_s, :count => 1
+    assert_select 'li>span.host', :text => 'Host City'.to_s, :count => 2
+    assert_select 'tr>td', :text => false.to_s, :count => 2
   end
 end
