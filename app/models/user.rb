60b621e3d2b04484590b848f68ec4296db89fc27
Colin Bruce <colinbruce@gmail.com>
2015-02-22 12:55:27 +0000

    update user details on login

--- app/models/user.rb
+++ app/models/user.rb
@@ -13,6 +13,11 @@ class User < ActiveRecord::Base
   def self.find_for_google_oauth2(access_token, signed_in_resource=nil)
     data = access_token.info
     user = User.where(:email => data['email']).first
+    if user
+      user.update!(name: data['name'],
+                   encrypted_password: Devise.friendly_token[0,20],
+                   image: data['image'])
+    end
 
     unless user
       user = User.create!(name: data['name'],
