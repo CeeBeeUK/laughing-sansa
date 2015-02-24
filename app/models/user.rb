36cdf6a09e6aac6b4f07a7a5ec9dfbe6748de3a6
Colin Bruce <colinbruce@gmail.com>
2015-02-24 18:27:47 +0000

    Added display_name property to user and tested

--- app/models/user.rb
+++ app/models/user.rb
@@ -9,7 +9,6 @@ class User < ActiveRecord::Base
   validates :role, presence: true
   validates :role, inclusion: { in: ROLES, message: "%{value} is not a valid role", allow_nil: true }
 
-
   def self.find_for_google_oauth2(access_token, signed_in_resource=nil)
     data = access_token.info
     user = User.where(:email => data['email']).first
@@ -32,6 +31,10 @@ class User < ActiveRecord::Base
       )
     end
     user
+  end
+
+  def display_name
+    self[:display_name] ||= self[:first_name]
   end
 
   def admin?
--- spec/factories/users.rb
+++ spec/factories/users.rb
@@ -2,6 +2,10 @@ FactoryGirl.define do
   factory :user do
     role 'user'
     sequence(:email){|n| "user#{n}@example.com" }
+    name 'test user'
+    first_name 'test'
+    last_name 'user'
+    display_name nil
     factory :admin_user do
       role 'admin'
       sequence(:email){|n| "admin#{n}@example.com" }
