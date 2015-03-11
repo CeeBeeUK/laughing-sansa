45c9df036bd20bc8668f890ebc71864eaac49e70
Colin Bruce <colinbruce@gmail.com>
2015-03-11 21:33:22 +0000

    rubocop amendments for requests/events_spec

--- spec/requests/events_spec.rb
+++ spec/requests/events_spec.rb
@@ -9,43 +9,47 @@ RSpec.describe 'Events', type: :request do
   let(:admin_user) { create(:admin_user) }
 
   describe 'GET /events' do
-    it 'should redirect when user not authenticated' do
+    it 'redirects when user not authenticated' do
       get events_path
       expect(response.status).to eql(302)
     end
 
-    it 'should return 200 when authenticated' do
-      login_as(user, :scope => :user)
+    it 'returns 200 when authenticated' do
+      login_as(user, scope: user)
       get events_path
       expect(response.status).to eql(200)
     end
 
-    it 'should return 200 when authorised' do
-      login_as(admin_user, :scope => :user)
+    it 'returns 200 when authorised' do
+      login_as(admin_user, scope: user)
       get events_path
       expect(response.status).to eql(200)
     end
   end
   describe 'GET /events/new' do
-    it 'Redirects us when not authenticated' do
+    it 'redirects when not authenticated' do
       get new_event_path
       expect(response.status).to eql(302)
     end
     context 'valid user' do
 
-      let(:countries) {assign(:countries, [
-                                            Country.create(name: 'country 1'),
-                                            Country.create(name: 'country 2')
-                                        ])}
+      let(:countries) {
+        assign(:countries,
+          [
+            Country.create(name: 'country 1'),
+            Country.create(name: 'country 2')
+          ]
+        )
+      }
 
-      it 'should return 200 when authenticated' do
-        login_as(user, :scope => :user)
+      it 'returns 200 when authenticated' do
+        login_as(user, scope: user)
         get new_event_path
         expect(response.status).to eql(302)
       end
 
-      it 'should return 200 when authorised' do
-        login_as(admin_user, :scope => :user)
+      it 'returns 200 when authorised' do
+        login_as(admin_user, scope: user)
         get new_event_path
         expect(response.status).to eql(200)
       end
