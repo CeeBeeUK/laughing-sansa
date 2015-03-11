1c7e8fadd15564b72ca212afac00bd0957d8d923
Colin Bruce <colinbruce@gmail.com>
2015-03-11 21:37:44 +0000

    rubocop amendments for routing/profile spec

--- spec/routing/profile_routing_spec.rb
+++ spec/routing/profile_routing_spec.rb
@@ -1,10 +1,10 @@
-require "rails_helper"
+require 'rails_helper'
 
-RSpec.describe ProfileController, :type => :routing do
-  describe "routing" do
+RSpec.describe ProfileController, type: :routing do
+  describe 'routing' do
 
-    it "routes to #show" do
-      expect(:get => "/my_profile").to route_to("profile#show")
+    it 'routes to #show' do
+      expect(get: '/my_profile').to route_to('profile#show')
     end
   end
 end
