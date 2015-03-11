7984e8f10a311704c1d5e935dca3dec1beac707c
Colin Bruce <colinbruce@gmail.com>
2015-03-11 22:47:46 +0000

    rubocop amendmends for user model specs

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -1,66 +1,66 @@
-require 'spec_helper'
+require 'rails_helper'
 
-describe User, :type => :model do
+RSpec.describe User, type: :model do
 
   let(:user)          { build(:user) }
   let(:admin_user)    { build(:admin_user) }
 
-  it 'should pass factory build' do
+  it 'passes factory build' do
     expect(user).to be_valid
   end
 
-  describe 'validations' do
-    it 'should require an email' do
+  describe 'validation' do
+    it 'requires an email' do
       user.email = nil
       expect(user).to be_invalid
     end
 
-    it 'should require a valid email' do
+    it 'requires a valid email' do
       user.email = 'testemail'
       expect(user).to be_invalid
     end
 
-    it 'should require a unique email' do
-      User.create({
-                    email: 'test@duplicate.com',
-                    role: 'user'
-                  })
-      duplicate = User.new({
-                             role: 'admin',
-                             email: 'test@duplicate.com'
-                           })
+    it 'requires a unique email' do
+      described_class.create(
+        email: 'test@duplicate.com',
+        role: 'user'
+      )
+      duplicate = described_class.new(
+        role: 'admin',
+        email: 'test@duplicate.com'
+      )
       expect(duplicate).to be_invalid
     end
 
-    it 'should require a non-nil role' do
+    it 'requires a non-nil role' do
       user.role = nil
       expect(user).to be_invalid
       expect(user.errors[:role]).to eq ["can't be blank"]
     end
 
-    it 'should require a valid role' do
+    it 'requires a valid role' do
       user.role = 'student'
       expect(user).to be_invalid
       expect(user.errors[:role]).to eq ['student is not a valid role']
     end
 
-    it 'should return the oaauth first name if display name empty' do
+    it 'returns the oaauth first name if display name empty' do
       user.display_name = nil
       expect(user.display_name).to eql('test')
     end
 
-    it 'should return the display name if explicitly set' do
+    it 'returns the display name if explicitly set' do
       user.display_name = 'wibble'
       expect(user.display_name).to eql('wibble')
     end
   end
   describe '@display_name_set?' do
-    it 'should respond true if a name is explicitly set' do
-      user.display_name='CeeBee'
+    it 'responds true if a name is explicitly set' do
+      user.display_name = 'CeeBee'
       expect(user.display_name_set?).to be true
     end
 
-    it 'should respond false if not admin user' do
+    it 'responds false if not admin user' do
       user.display_name = nil
       expect(user.display_name_set?).to be false
       user.display_name = ''
@@ -68,38 +68,38 @@ describe User, :type => :model do
     end
   end
   describe '@admin?' do
-    it 'should respond true if admin user' do
+    it 'responds true if admin user' do
       expect(admin_user.admin?).to be true
     end
 
-    it 'should respond false if not admin user' do
+    it 'responds false if not admin user' do
       expect(user.admin?).to be false
     end
   end
   it 'can find existing user' do
-    User.create({
-                    email: 'test_create@example.com',
-                    name: 'Test User',
-                    role: 'user'
-                })
-    user_struct = OpenStruct.new({
-                info: {
-                    'email'=> 'test_create@example.com',
-                    'name' => 'Test User'
-                }
-            })
-    user2 = User.find_for_google_oauth2(user_struct)
+    described_class.create(
+      email: 'test_create@example.com',
+      name: 'Test User',
+      role: 'user'
+    )
+    user_struct = OpenStruct.new(
+      info: {
+        'email' => 'test_create_unique_27@example.com',
+        'name' => 'Test User'
+      }
+    )
+    user2 = described_class.find_for_google_oauth2(user_struct)
     expect(user2).to be_valid
   end
   it 'will create a new user if credentials valid' do
-    user_struct = OpenStruct.new({
-                                     info: {
-                                         'email'=> 'test_create_unique@example.com',
-                                         'name' => 'Test User'
-                                     }
-                                 })
+    user_struct = OpenStruct.new(
+      info: {
+        'email' => 'test_create_unique@example.com',
+        'name' => 'Test User'
+      }
+    )
     expect {
-      User.find_for_google_oauth2(user_struct)
-    }.to change(User, :count).by(1)
+      described_class.find_for_google_oauth2(user_struct)
+    }.to change(described_class, :count).by(1)
   end
 end
