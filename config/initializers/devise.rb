130e2542cb33b6abc4719f22f2d5d7f40e4ae6eb
Colin Bruce <colinbruce@gmail.com>
2015-03-12 09:14:08 +0000

    Update devise.rb

--- config/initializers/devise.rb
+++ config/initializers/devise.rb
@@ -274,11 +274,11 @@ Devise.setup do |config|
   # up on your models and hooks.
   # config.omniauth :github, 'APP_ID', 'APP_SECRET', scope: 'user,public_repo'
   config.omniauth :google_oauth2, ENV["GOOGLE_CLIENT_ID"], ENV["GOOGLE_CLIENT_SECRET"],
-      scope: 'email, profile',
-      prompt: 'select_account',
-      access_type: 'online',
-      image_aspect_ratio: "square",
-      image_size: 25
+    scope: 'email, profile',
+    prompt: 'select_account',
+    access_type: 'online',
+    image_aspect_ratio: "square",
+    image_size: 25
 
   # ==> Warden configuration
   # If you want to use other strategies, that are not supported by Devise, or
