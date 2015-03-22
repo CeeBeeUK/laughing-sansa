d595fd13b23d8d4f4e88bca9b2fd52f21ec9f1c7
Colin Bruce <colinbruce@gmail.com>
2015-03-22 18:05:12 +0000

    Updated tests to replace sequence with position

--- app/models/participating_country.rb
+++ app/models/participating_country.rb
@@ -5,7 +5,7 @@ class ParticipatingCountry < ActiveRecord::Base
 
   acts_as_list scope: :event
 
-  validates :country_id, :player_id, :event_id, :sequence, presence: true
-  validates :sequence, uniqueness: { scope: :event_id }
+  validates :country_id, :player_id, :event_id, :position, presence: true
+  validates :position, uniqueness: { scope: :event_id }
   validates :country_id, uniqueness: { scope: :event_id }
 end
