f737e9cb80cf60db34b0394f0a41214dcd6a4410
Colin Bruce <colinbruce@gmail.com>
2015-04-01 22:46:15 +0100

    Added test for routing scores

--- spec/routing/my_routing_spec.rb
+++ spec/routing/my_routing_spec.rb
@@ -1,15 +1,18 @@
 require 'rails_helper'
 
 RSpec.describe MyController, type: :routing do
-  describe 'routing' do
-    it 'routes to #profile' do
+  describe 'routes' do
+    it '#profile' do
       expect(get: 'my/profile').to route_to('my#profile')
     end
-    it 'routes to #update on patch' do
+    it 'to #update on patch' do
       expect(patch: 'my/profile').to route_to('my#profile_update')
     end
-    it 'routes to #game' do
+    it '#game' do
       expect(get: 'my/2015/game').to route_to('my#game', year: '2015')
+    end
+    it '#score' do
+      expect(get: 'my/2015/score/act/1').to route_to('my#score', year: '2015', act: '1')
     end
   end
 end
