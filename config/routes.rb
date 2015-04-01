a1a2e248d1ec948923dd3324f055346e188fd02f
Colin Bruce <colinbruce@gmail.com>
2015-04-01 22:46:39 +0100

    Added route for scoring

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,7 @@
 Rails.application.routes.draw do
 
+  get 'my/:year/score/act/:act', to: 'my#score', as: 'my_scoring'
+
   get 'events/:year/join', to: 'events#join', as: 'game_join'
   post 'events/:year/join', to: 'events#sign_up', as: 'game_sign_up'
 
