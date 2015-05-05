8142c35f0ab6ea8ae7fc52a11f9db59d57877518
Colin Bruce <colinbruce@gmail.com>
2015-05-05 21:22:28 +0100

    removed un-needed code block

--- app/controllers/users/omniauth_callbacks_controller.rb
+++ app/controllers/users/omniauth_callbacks_controller.rb
@@ -4,8 +4,6 @@ class Users::OmniauthCallbacksController < Devise::OmniauthCallbacksController
     if @user.persisted?
       flash[:notice] = I18n.t 'devise.omniauth_callbacks.success', kind: 'Google'
       sign_in_and_redirect @user, event: :authentication
-    else
-      session['devise.google_data'] = oauth_request
     end
   end
 
