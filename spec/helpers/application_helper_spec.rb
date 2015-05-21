fd2ecab63e5685395f832f684f5842bd6b11b688
Colin Bruce <colinbruce@gmail.com>
2015-05-21 23:13:57 +0100

    Updated time xone in test

--- spec/helpers/application_helper_spec.rb
+++ spec/helpers/application_helper_spec.rb
@@ -3,9 +3,9 @@ require 'rails_helper'
 RSpec.describe ApplicationHelper do
   describe 'timeago' do
     it 'renders an abbr' do
-      display = DateTime.new(2015, 5, 5, 12, 30).utc.to_time
-      expected = '<abbr class="timeago" title="2015-05-05T12:30:00Z">2015-05-05 13:30:00 +0100</abbr>'
-      expect(helper.timeago(display)).to eql(expected)
+      start = DateTime.new(2015, 5, 5, 12, 30).to_time
+      expected = "<abbr class=\"timeago\" title=\"#{start.getutc.iso8601}\">#{start}</abbr>"
+      expect(helper.timeago(start)).to eql(expected)
     end
   end
 end
