fdd8e4539eaf61a2ab47fe7b710908d47a5130e7
Colin Bruce <colinbruce@gmail.com>
2015-06-17 12:20:36 +0100

    Update application_helper_spec.rb

--- spec/helpers/application_helper_spec.rb
+++ spec/helpers/application_helper_spec.rb
@@ -3,7 +3,8 @@ require 'rails_helper'
 RSpec.describe ApplicationHelper do
   describe 'timeago' do
     it 'renders an abbr' do
-      start = DateTime.new(2015, 5, 5, 12, 30).to_time
+      Time.zone = 'UTC'
+      start = Time.zone.local(2015, 5, 5, 12, 30)
       expected = "<abbr class=\"timeago\" title=\"#{start.getutc.iso8601}\">#{start}</abbr>"
       expect(helper.timeago(start)).to eql(expected)
     end
