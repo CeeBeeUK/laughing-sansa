fe6ac6c794b05f52456e1a878c9f4f6cafa378f5
CeeBeeUK <colinbruce@gmail.com>
2016-05-09 15:19:18 +0100

    Working route for admin page

--- config/routes.rb
+++ config/routes.rb
@@ -3,6 +3,7 @@ Rails.application.routes.draw do
   get 'my/:year/score/act/:act', to: 'my#score', as: 'my_scoring'
   patch 'my/:year/score/act/:act', to: 'my#score_create'
 
+  get 'events/:year/admin', to: 'events#admin', as: 'game_admin'
   get 'events/:year/join', to: 'events#join', as: 'game_join'
   post 'events/:year/join', to: 'events#sign_up', as: 'game_sign_up'
 
