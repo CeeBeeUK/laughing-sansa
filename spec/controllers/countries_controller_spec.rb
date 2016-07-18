1d291e069779dfb0afab851b984c374c5ea07b52
CeeBeeUK <colinbruce@gmail.com>
2016-07-18 23:01:21 +0100

    updated countries_controller tests

--- spec/controllers/countries_controller_spec.rb
+++ spec/controllers/countries_controller_spec.rb
@@ -20,7 +20,7 @@ RSpec.describe CountriesController, type: :controller do
     describe 'GET index' do
       it 'assigns all countries as @countries' do
         country = Country.create! valid_attributes
-        get :index, {}, valid_session
+        get :index, params: {}, session: valid_session
         expect(assigns(:countries)).to include(country)
       end
     end
@@ -28,14 +28,14 @@ RSpec.describe CountriesController, type: :controller do
     describe 'GET show' do
       it 'assigns the requested country as @country' do
         country = Country.create! valid_attributes
-        get :show, { name: country.to_param }, valid_session
+        get :show, params: { name: country.to_param }, session: valid_session
         expect(assigns(:country)).to eq(country)
       end
     end
 
     describe 'GET new' do
       it 'assigns a new country as @country' do
-        get :new, {}, valid_session
+        get :new, params: {}, session: valid_session
         expect(assigns(:country)).to be_a_new(Country)
       end
     end
@@ -43,7 +43,7 @@ RSpec.describe CountriesController, type: :controller do
     describe 'GET edit' do
       it 'assigns the requested country as @country' do
         country = Country.create! valid_attributes
-        get :edit, { name: country.to_param }, valid_session
+        get :edit, params: { name: country.to_param }, session: valid_session
         expect(assigns(:country)).to eq(country)
       end
     end
@@ -52,30 +52,30 @@ RSpec.describe CountriesController, type: :controller do
       describe 'with valid params' do
         it 'creates a new Country' do
           expect do
-            post :create, { country: valid_attributes }, valid_session
+            post :create, params: { country: valid_attributes }, session: valid_session
           end.to change(Country, :count).by(1)
         end
 
         it 'assigns a newly created country as @country' do
-          post :create, { country: valid_attributes }, valid_session
+          post :create, params: { country: valid_attributes }, session: valid_session
           expect(assigns(:country)).to be_a(Country)
           expect(assigns(:country)).to be_persisted
         end
 
         it 'redirects to the created country' do
-          post :create, { country: valid_attributes }, valid_session
+          post :create, params: { country: valid_attributes }, session: valid_session
           expect(response).to redirect_to(Country.last)
         end
       end
 
       describe 'with invalid params' do
         it 'assigns a newly created but unsaved country as @country' do
-          post :create, { country: invalid_attributes }, valid_session
+          post :create, params: { country: invalid_attributes }, session: valid_session
           expect(assigns(:country)).to be_a_new(Country)
         end
 
         it 're-renders the "new" template' do
-          post :create, { country: invalid_attributes }, valid_session
+          post :create, params: { country: invalid_attributes }, session: valid_session
           expect(response).to render_template('new')
         end
       end
@@ -87,20 +87,20 @@ RSpec.describe CountriesController, type: :controller do
 
         it 'updates the requested country' do
           country = Country.create! valid_attributes
-          put :update, { name: country.to_param, country: new_attributes }, valid_session
+          put :update, params: { name: country.to_param, country: new_attributes }, session: valid_session
           country.reload
           expect(country.name).to eql('uk')
         end
 
         it 'assigns the requested country as @country' do
           country = Country.create! valid_attributes
-          put :update, { name: country.to_param, country: valid_attributes }, valid_session
+          put :update, params: { name: country.to_param, country: valid_attributes }, session: valid_session
           expect(assigns(:country)).to eq(country)
         end
 
         it 'redirects to the country' do
           country = Country.create! valid_attributes
-          put :update, { name: country.to_param, country: valid_attributes }, valid_session
+          put :update, params: { name: country.to_param, country: valid_attributes }, session: valid_session
           expect(response).to redirect_to(country)
         end
       end
@@ -108,13 +108,13 @@ RSpec.describe CountriesController, type: :controller do
       describe 'with invalid params' do
         it 'assigns the country as @country' do
           country = Country.create! valid_attributes
-          put :update, { name: country.to_param, country: invalid_attributes }, valid_session
+          put :update, params: { name: country.to_param, country: invalid_attributes }, session: valid_session
           expect(assigns(:country)).to eq(country)
         end
 
         it 're-renders the "edit" template' do
           country = Country.create! valid_attributes
-          put :update, { name: country.to_param, country: invalid_attributes }, valid_session
+          put :update, params: { name: country.to_param, country: invalid_attributes }, session: valid_session
           expect(response).to render_template('edit')
         end
       end
@@ -124,13 +124,13 @@ RSpec.describe CountriesController, type: :controller do
       it 'destroys the requested country' do
         country = Country.create! valid_attributes
         expect do
-          delete :destroy, { name: country.to_param }, valid_session
+          delete :destroy, params: { name: country.to_param }, session: valid_session
         end.to change(Country, :count).by(-1)
       end
 
       it 'redirects to the countries list' do
         country = Country.create! valid_attributes
-        delete :destroy, { name: country.to_param }, valid_session
+        delete :destroy, params: { name: country.to_param }, session: valid_session
         expect(response).to redirect_to(countries_url)
       end
     end
