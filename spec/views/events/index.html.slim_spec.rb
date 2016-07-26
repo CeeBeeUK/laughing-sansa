01e108e24baafc1495e1293eeae456272fdb7b58
CeeBeeUK <colinbruce@gmail.com>
2016-07-26 22:42:05 +0100

    Update view tests

--- spec/views/events/index.html.slim_spec.rb
+++ spec/views/events/index.html.slim_spec.rb
@@ -14,11 +14,11 @@ RSpec.describe 'events/index', type: :view do
         Event.create!(year: 2011, host_city: 'Host City', active: false, country: country, status: 'archived', real_winner: country, home_winner: country, real_player_name: 'bob', home_player_name: 'bob'),
         Event.create!(year: 2012, host_city: 'Host City', active: true, country: country, status: 'setup', real_winner: nil, home_winner: nil, real_player_name: nil, home_player_name: nil, date: Time.zone.today)
       ])
+    sign_in user
+    render
   end
 
   it 'renders a list of events' do
-    sign_in user
-    render
     assert_select 'li span.year', text: 2010.to_s, count: 1
     assert_select 'li span.year', text: 2011.to_s, count: 1
     assert_select 'li span.host-city', text: 'Host City'.to_s, count: 3
