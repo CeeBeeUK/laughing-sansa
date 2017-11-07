4688cb7df43d7581a56b63094b1d72f897300a16
CeeBeeUK <colinbruce@gmail.com>
2017-11-07 16:50:22 +0000

    big splotch 2 the revenge

--- spec/features/setting_most_spec.rb
+++ spec/features/setting_most_spec.rb
@@ -1,12 +1,43 @@
 require 'rails_helper'
 
 feature 'user scoring' do
-  scenario 'sets a performance to fattest' do
-    event = create(:event)
-    player = create(:event_player, event: event)
+  include Warden::Test::Helpers
+  Warden.test_mode!
+  let(:user) { create(:user) }
+  let(:event) { create(:event, :with_countries) }
+  let(:player) { create(:event_player, event: event, player: user) }
+  let(:eps_1) { EventPlayerScore.find_by(event_player: player, participating_country: event.participating_countries.first) }
+  let(:eps_2) { EventPlayerScore.find_by(event_player: player, participating_country: event.participating_countries.second) }
 
+  before do
+    OmniAuth.config.test_mode = true
+    OmniAuth.config.mock_auth[:default] = OmniAuth::AuthHash.new(
+      provider: 'google_oauth2',
+      uid: '123456789',
+      info: {
+        name: 'John Doe',
+        email: 'john@company_name.com',
+        first_name: 'John',
+        last_name: 'Doe',
+        image: 'https://lh3.googleusercontent.com/url/photo.jpg'
+      }
+    )
+    allow(User).to receive(:find_for_google_oauth2).and_return(user)
+    eps_1.update_attributes(score: 5, wackiest: true)
+  end
+
+  scenario 'sets a performance to wackiest' do
     visit root_path
     click_link :login
-    expect(page).to have_content('event')
+    expect(page).to have_xpath("//div[contains(., 'Successfully authenticated from Google account')] ")
+    expect(page).to have_xpath("//input[@type='text' and @value='1/3 acts']")
+    click_link 'score_act_2'
+    check 'event_player_score_wackiest'
+    choose 'event_player_score_score_7', visible: false
+    click_button 'Submit score'
+    eps_1.reload
+    eps_2.reload
+    expect(eps_2.wackiest).to be true
+    expect(eps_1.wackiest).to be false
   end
 end
