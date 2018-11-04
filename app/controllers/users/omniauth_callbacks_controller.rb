5fbc24eb352f5a31c0d658796a1aceb39947f2e6
Colin Bruce <colinbruce@gmail.com>
2018-11-04 15:48:39 +0000

    Bump rubocop (#215)
    * update rubocop gem
    * Address Layout/EmptyLineAfterGuardClause cop

--- app/controllers/users/omniauth_callbacks_controller.rb
+++ app/controllers/users/omniauth_callbacks_controller.rb
@@ -3,6 +3,7 @@ module Users
     def google_oauth2
       @user = User.find_for_google_oauth2(oauth_request, current_user)
       return unless @user.persisted?
+
       flash[:notice] = I18n.t 'devise.omniauth_callbacks.success', kind: 'Google'
       sign_in_and_redirect @user, event: :authentication
     end
