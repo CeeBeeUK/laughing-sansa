28b40e778d82346d680c0c48933ab1dd3285bb7d
Colin Bruce <colinbruce@gmail.com>
2021-08-20 07:16:14 +0100

    Update Big5 array to make rubocop happy

--- lib/global_constants.rb
+++ lib/global_constants.rb
@@ -1,4 +1,4 @@
 module GlobalConstants
   VALID_SCORES = [*0..8, 10, 12].freeze
-  BIG5 = %w[United\ Kingdom Italy Spain Germany France].freeze
+  BIG5 = ['United Kingdom', 'Italy', 'Spain', 'Germany', 'France'].freeze
 end
