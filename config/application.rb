66c3823958c8ef4eb4c4121b03612cf178ff18fb
Colin Bruce <colinbruce@gmail.com>
2021-12-18 19:23:56 +0000

    Rails 7: Update config
    set config.active_record.legacy_connection_handling = false to reduce warnings

--- config/application.rb
+++ config/application.rb
@@ -40,5 +40,6 @@ module Eurovision
     # keep the name alive
     config.middleware.use Rack::Pratchett
     config.action_dispatch.return_only_media_type_on_content_type = false
+    config.active_record.legacy_connection_handling = false
   end
 end
