b6e06ed4e689afe4e05f94cdf0abbec39f4b806f
Colin Bruce <colinbruce@gmail.com>
2022-04-30 21:24:11 +0100

    Update autoloaders ignore

--- config/application.rb
+++ config/application.rb
@@ -41,5 +41,8 @@ module Eurovision
     config.middleware.use Rack::Pratchett
     config.action_dispatch.return_only_media_type_on_content_type = false
     config.active_record.legacy_connection_handling = false
+    Rails.autoloaders.main.ignore(Rails.root.join('/app/app/webpacker/images/16'))
+    Rails.autoloaders.main.ignore(Rails.root.join('/app/app/webpacker/images/32'))
+    Rails.autoloaders.main.ignore(Rails.root.join('/app/app/webpacker/images/64'))
   end
 end
